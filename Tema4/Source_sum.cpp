41d9812cacbf1df63d4dde1385ea2f260346da05
Arkin1 <stoicaastefan@gmail.com>
2018-04-02 00:04:36 +0300

    Update Source_sum.cpp

--- Tema4/Source_sum.cpp
+++ Tema4/Source_sum.cpp
@@ -4,85 +4,93 @@ using namespace std;
 
 const int nr = 3;
 
-int v[10] = { 4,1,2,3,3,3,3,10,11,12 };
+int array_size=10;
 int main(int argc, char **argv)
-{   
-	
+{
+
 	int  numtasks, rank, len, rc;
 	char hostname[MPI_MAX_PROCESSOR_NAME];
-	
-	rc = MPI_Init(&argc,&argv);
+
+	rc = MPI_Init(&argc, &argv);
 
 	if (rc != MPI_SUCCESS)
-	{     
-		printf ("Error starting MPI program. Terminating.\n");
+	{
+		printf("Error starting MPI program. Terminating.\n");
 		MPI_Abort(MPI_COMM_WORLD, rc);
 	}
 
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(hostname, &len);
 
-	int array_size = sizeof(v) / sizeof(int);
-
-	int del[2];
-
-	MPI_Request request;
-
-
-
-	MPI_Irecv(&del, 2, MPI_INT, 0, 1, MPI_COMM_WORLD, &request);
+	
 
 	if (rank == 0)
 	{
-		int nr = array_size / numtasks;
-		int r = array_size % numtasks;
-		int del[2];
-		del[0] = del[1] = 0;
-		
-		for (int i = 0; i < numtasks; ++i)
+		int nr = array_size / (numtasks-1);
+		int r = array_size % (numtasks-1);
+		int x[100], ind = 0;
+		for (int i = 1; i < numtasks; ++i)
 		{
-			del[1] = del[0] + nr + (r>0) - 1;
-			MPI_Send(&del, 2, MPI_INT, i, 1, MPI_COMM_WORLD);
+			for (int j = 1; j <= nr + (r > 0); ++j)
+			{
+				x[ind] = rand();
 
-			del[0] = del[1] + 1;
+				cout << x[ind]<<" ";
+
+				ind++;
+			}
+
+			MPI_Send(&ind, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+
+			MPI_Send(&x, ind, MPI_INT, i, 1, MPI_COMM_WORLD);
 
 			if (r > 0)
 				--r;
+
+
+			ind = 0;
+
 		}
-		
+
 	}
-	MPI_Status status;
 
-	MPI_Wait(&request, &status);
-
-	printf("My rank= %d del=(%d %d)\n", rank, del[0], del[1]);
-
-	int sum = 0;
-	
-	for (int i = del[0]; i <= del[1]; ++i)
+	if (rank != 0)
 	{
-		
-		sum += v[i];
-	
-	}
+		int sz;
+		int x[100];
 
-	MPI_Isend(&sum, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &request);
+		MPI_Recv(&sz, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&x, sz, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		int sum = 0;
+
+		for (int i = 0; i < sz; ++i)
+		{
+
+			sum += x[i];
+
+		}
+
+		MPI_Send(&sum, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	}
+	
+
 
 	if (rank == 0)
 	{
 		int sum = 0;
-		for(int i=0;i < numtasks;++i)
+		for (int i = 1; i < numtasks; ++i)
 		{
 			int nr;
-			MPI_Recv(&nr, 1, MPI_INT , MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			
+			MPI_Recv(&nr, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
 			sum += nr;
 		}
 
-		cout << sum;
+		cout <<'\n'<< sum;
 	}
-	
+
 
 	MPI_Finalize();
 
